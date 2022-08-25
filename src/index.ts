65153f0ec393b0a94a622a81ec14e93ce17ac4ab
duncan <dwerner@riskamp.com>
2022-08-25 11:36:43 -0700

    add singleton instance

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 
+import { Highlighter } from './highlight.js';
+
 /** remark/rehype plugin */
 export { CompositePlugin } from './plugins.js';
 
@@ -8,3 +10,15 @@ export type { Meta } from './meta.js';
 /** direct calls for special rendering */
 export { Highlighter } from './highlight.js';
 
+/**
+ * create an instance that we can cache, this will simplify
+ * using it from an astro component
+ */
+let singleton: Highlighter;
+
+export const GetInstance = () => {
+  if (!singleton) {
+    singleton = new Highlighter();
+  }
+  return singleton;
+}
